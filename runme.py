c73e6529317b5681af0a3069a446d9751fbe9e6e
alex <alexgalayda@gmail.com>
2021-03-16 14:55:45 +0300

    hot fix

--- runme.py
+++ runme.py
@@ -8,9 +8,8 @@ from polygon.gui_main import MainGUI
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--dst', default=None, help='Full name for saving result (default: save to dir with img)')
-    parser.add_argument('--scale', '-s', type=int, default=10, help='Scale coeficente')
-    #args = parser.parse_args(['--dst', './output/test.json'])
+    parser.add_argument('--scale', '-s', type=int, default=10, help='Scale coeficente (default: 10)')
     args = parser.parse_args()
     
-    app = MainGUI(tk.Tk(), args.dst, args.scale)  # start the application
-    app.mainloop()  # application is up and running
+    app = MainGUI(tk.Tk(), args.dst, args.scale)
+    app.mainloop()
